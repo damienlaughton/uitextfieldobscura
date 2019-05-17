768b2fbbafc7bf338c6bf04c6e468fc0477bd176
Damo <dfplaughton@googlemail.com>
2019-05-17 08:53:41 +0100

    Initial single view project created

---
+++ uitextfieldobscura/uitextfieldobscura/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uitextfieldobscura
+//
+//  Created by Damien Laughton on 17/05/2019.
+//  Copyright © 2019 Damien Laughton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
